9c2c65021a8620b4fb3f8ece3b4a7ffd8e0588cb
Antoine Neuenschwander <antoine.neuenschwander@ergon.ch>
2015-03-26 19:01:22 +0100

    fixes to exercises 00 and 01

--- level05/exercise00.php
+++ level05/exercise00.php
@@ -38,7 +38,7 @@
         
         // name is a required field
         if (empty($name)) {
-            array_push($result["errors"], array("name" => "name is required")); 
+            array_push($result["errors"], array("field" => "name", "message" => "name is required")); 
             return false;
         }
         
@@ -46,7 +46,7 @@
         // (this is actually a fake check,
         // it returns true if the letter a is found in the name).
         if (strpos($name, 'a') !== false) {
-            array_push($result["errors"], array("name" => "name already used"));
+            array_push($result["errors"], array("field" => "name", "message" => "name already used"));
             return false;
         }
         
@@ -59,13 +59,13 @@
         
         // email is a required field
         if (empty($email)) {
-            array_push($result["errors"], array("email" => "email is required"));
+            array_push($result["errors"], array("field" => "email", "message" => "email is required"));
             return false;
         }
         
         // reject invalid email format
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            array_push($result["errors"], array("email" => "invalid email format"));
+            array_push($result["errors"], array("field" => "email", "message" => "invalid email format"));
             return false;   
         }
         
@@ -78,7 +78,7 @@
         
         // comment cannot be longer than 50 characters
         if (strlen($comment) > 50) {
-            array_push($result["errors"], array("comment" => "comment is too long"));
+            array_push($result["errors"], array("field" => "comment", "message" => "comment is too long"));
             return false;
         }
         
@@ -92,7 +92,7 @@
         if (!empty($website)) {
             // reject invalid URL format
             if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-                array_push($result["errors"], array("website" => "invalid URL"));
+                array_push($result["errors"], array("field" => "website", "message" => "invalid URL"));
                 return false;
             }
         }
--- level05/exercise01.php
+++ level05/exercise01.php
@@ -1,14 +1,11 @@
 <?php
 if (isset($_REQUEST['query'])) {
-$query = $_REQUEST['query'];
-$callback = $_REQUEST['callback'];
-$countries = file("exercise01_countries.txt");
-$result = array_map('trim', array_values(preg_grep("/^$query/i", $countries)));
-header('Content-Type', 'application/json');
-if ($callback) {
-	echo $callback.'('.json_encode($result).')';
-} else {
-	echo json_encode($result);
-}
+    $query = $_REQUEST['query'];
+    
+    $countries = file("exercise01_countries.txt");
+    $result = array_map('trim', array_values(preg_grep("/^$query/i", $countries)));
+    
+    header('Content-Type', 'application/json');
+    echo json_encode($result);
 }
 ?>
